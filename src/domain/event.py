68eb06b8dba203ed09413443b8e5b453bd06b0b6
jm5905938 <jm5905938@gmail.com>
2026-09-11 06:17:10 +0000

    feat(domain):定义了“event”模型

---
+++ src/domain/event.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass, field
+from datetime import datetime, timezone
+from uuid import UUID, uuid4
+
+
+@dataclass
+class Event:
+    """
+    runtime事件对象
+    """
+
+    type: str
+    payload: dict
+    id: UUID = field(default_factory=uuid4)
+    created_at: datetime = field(default_factory=lambda: datetime.now(timezone.utc))
